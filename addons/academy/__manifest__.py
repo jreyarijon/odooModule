30fbd0bf19f5dad88bf8f1e8e8a8b04f82be53be
jreyarijon <jreyarijon@danielcastelao.org>
2021-03-14 14:06:52 +0100

    Creado openacademy.xml y añadido a la data list del manifest

--- addons/academy/__manifest__.py
+++ addons/academy/__manifest__.py
@@ -25,8 +25,8 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'templates.xml',
+        'views/openacademy.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
